ddf55a947bb6d05b06cf23c31df402735eb3e1cd
Dittocopy <71779263+Dittocopy@users.noreply.github.com>
2021-12-24 22:58:54 +0530

    update in api

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -1,27 +1,47 @@
 package com.example.demo.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.CheckoutData;
 import com.example.demo.entity.UserData;
+import com.example.demo.repository.CheckRepository;
 import com.example.demo.repository.UserRepository;
-
 
 @RestController
 @RequestMapping("/sign")
 
+// API Controller
 public class ApiController {
-@Autowired
-UserRepository ss;
-@GetMapping("/users")
-public List<UserData>get(){
-return ss.findAll();
-}
+	@Autowired
+	UserRepository Us;
 
-   
+	@GetMapping("/users")
+	public List<UserData> get() {
+		return Us.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public Optional<UserData> getId(@PathVariable Integer id) {
+		return Us.findById(id);
+	}
+
+	@Autowired
+	CheckRepository Cr;
+
+	@GetMapping("/check/{id}")
+	public Optional<CheckoutData> gatadataId(@PathVariable Integer id) {
+		return Cr.findById(id);
+	}
+
+	@GetMapping("/check")
+	public List<CheckoutData> gatadata() {
+		return Cr.findAll();
+	}
 }
